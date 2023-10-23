df7522335e4e7d5287c489bae292437bcfeb4eff
ikechukwu kevin-chika <ikechukwukevin725@gmail.com>
2023-10-23 19:20:00 +0100

    update

--- js/main.js
+++ js/main.js
@@ -90,3 +90,25 @@ const currentDateElement = document.getElementById('currentDate');
         const formattedDate = currentDate.toLocaleDateString('en-US', options);
 
         currentDateElement.textContent = formattedDate;
+
+        function updateCurrentTime() {
+          // Create a new Date object to get the current date and time
+          var currentTime = new Date();
+
+          // Extract hours, minutes, and seconds
+          var hours = currentTime.getHours();
+          var minutes = currentTime.getMinutes();
+          var seconds = currentTime.getSeconds();
+
+          // Format the time as HH:MM:SS
+          var formattedTime = hours + ':' + (minutes < 10 ? '0' : '') + minutes + ':' + (seconds < 10 ? '0' : '') + seconds;
+
+          // Update the content of the "current-time" element
+          document.getElementById('current-time').textContent = formattedTime;
+      }
+
+      // Call the function initially to display the current time
+      updateCurrentTime();
+
+      // Update the current time every second (1000 milliseconds)
+      setInterval(updateCurrentTime, 1000);        
